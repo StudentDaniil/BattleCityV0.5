33c5ac8cf8c5443c0c09eb9885aaaf4e04a40c3d
StudentDaniil <99341865+StudentDaniil@users.noreply.github.com>
2022-12-25 21:40:27 +0300

    Add files via upload

---
+++ Battle City/src/com/GvozdevDaniil/game/EntityType.java
@@ -0,0 +1,7 @@
+package com.GvozdevDaniil.game;
+
+public enum EntityType {
+
+	Player, Enemy;
+
+}
---
+++ Battle City/src/com/GvozdevDaniil/main/Main.java
@@ -0,0 +1,14 @@
+package com.GvozdevDaniil.main;
+
+import com.GvozdevDaniil.game.Game;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Game tanks = new Game();
+		tanks.start();
+
+
+	}
+}
